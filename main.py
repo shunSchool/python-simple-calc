2991df32802d4a955081086f9c0ad7c5d908ef34
shunSchool <72032942+shunSchool@users.noreply.github.com>
2022-01-18 11:31:36 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#python simple calc
+
+#input
+num1 = int(input("please enter a number: "))
+num2 = int(input("please enter another number: "))
+
+#process
+total = num1 + num2
+
+#output
+print("the sum of the 2 numbers is", total, ".")
